2e2d21505d5d09c5e5ad0dcdcf22a358cd729b74
BorizBritva <10.95.karm@gmail.com>
2019-05-16 22:54:01 +0600

    controller code

--- script/script.js
+++ script/script.js
@@ -1,1 +1,8 @@
-const display = document.quarySelector('.display');
+const display = document.querySelector('.display'),
+	  gamePart = document.createElement('div'),
+	  pixel = document.createElement('span');
+
+gamePart.style.cssText = "width: 80%; \ height: 100%; \ border: 1px solid black; \ position: relative;";
+display.appendChild(gamePart);
+/*pixel.style.cssText = "width:"*/
+

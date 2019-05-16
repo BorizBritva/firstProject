050e4aef2d24e86041ffa988dd9589a390d69a51
BorizBritva <10.95.karm@gmail.com>
2019-05-16 23:34:47 +0600

    Changes

--- script/script.js
+++ script/script.js
@@ -1,8 +1,18 @@
 const display = document.querySelector('.display'),
 	  gamePart = document.createElement('div'),
-	  pixel = document.createElement('span');
+	  pixel = document.createElement('div');
 
 gamePart.style.cssText = "width: 80%; \ height: 100%; \ border: 1px solid black; \ position: relative;";
+pixel.style.cssText = "width: 10px; \ height: 10px; \ border: 1px solid black;";
+
 display.appendChild(gamePart);
+
+for (let i=40; i>0; i--) {
+	pixel.setAttribute("posX", i);
+	for (let j=1; j<=60; j++) {
+		pixel.setAttribute("posY", j);
+		gamePart.appendChild(pixel);
+	}
+}
 /*pixel.style.cssText = "width:"*/
 

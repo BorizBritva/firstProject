5b977911f9e8b8e7a715b111290c84e045776f62
BorizBritva <10.95.karm@gmail.com>
2019-05-17 01:03:45 +0600

    restart

--- script/script.js
+++ script/script.js
@@ -6,13 +6,8 @@ gamePart.style.cssText = "width: 80%; \ height: 100%; \ border: 1px solid black;
 pixel.style.cssText = "width: 10px; \ height: 10px; \ border: 1px solid black;";
 
 display.appendChild(gamePart);
-
-for (let i=40; i>0; i--) {
-	pixel.setAttribute("posX", i);
-	for (let j=1; j<=60; j++) {
-		pixel.setAttribute("posY", j);
-		gamePart.appendChild(pixel);
-	}
+for (let i=0; i<10; i++) {
+	gamePart.appendChild(pixel.setAttribute('posx', i));
 }
 /*pixel.style.cssText = "width:"*/
 

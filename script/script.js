5bc0fb0f4cd280ce8f2959e0f595a75f17594328
BorizBritva <10.95.karm@gmail.com>
2019-05-17 22:51:39 +0600

    game commit 1

--- script/script.js
+++ script/script.js
@@ -5,21 +5,42 @@ const display = document.querySelector('.display'),
 gamePart.style.cssText = "width: 80%; \ height: 100%; \ border: 1px solid black; \ position: relative; \ display: flex; \ flex-wrap: wrap;";
 //push pixel-elem to matrix
 for (let i=0; i<840; i++) {
-	let pixel = document.createElement('div');
-	pixel.classList.add('pixel');
-	gamePart.appendChild(pixel);
+	let elem = document.createElement('div');
+	elem.classList.add('pixel');
+	gamePart.appendChild(elem);
 };
 
 display.appendChild(gamePart);
 
 const pixel = document.querySelectorAll('.pixel');
 
-let index = 0;
-
-for (var y=42; y>0; y--) {
-	for (var x=1; x<22; x++) {
-		pixel[index].setAttribute('posY', y);
-		pixel[index].setAttribute('posX', x);
-		index++; 
-	}
+let n = 0;
+//индексация пикселей монитора
+for (let y=42; y>0; y--) {
+	for (let x=1; x<22; x++) {
+		pixel[n].setAttribute('posy',y);
+		pixel[n].setAttribute('posx',x);
+		n++;
+	};
 };
+
+//игровые объекты
+const Person = {
+	tetris: {
+		stick: [[0, 0],[0, 1],[0, -1]],
+		lightningLeft: [[0, 0], [0, 1], [-1, 0], [1, 1]],
+		lightningRight: [[0, 0], [0, 1], [-1, 1], [1, 0]],
+		tank: [[0, 0], [0, 1], [-1, 0], [1, 0]],
+		square: [] },
+	snake: [[0, 0], [-1, 0], [1, 0]],
+	tank: [[0, 0], [0, 1], [-1, 0], [1, 0]],
+	createFigure() {},
+	moveUp() {},
+	moveDown() {},
+	moveLeft() {},
+	moveRight() {}
+};
+
+
+
+
